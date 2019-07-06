5de2ff7107750a6e3e7549733027c8c77f9f4cc7
Lucas James <lucas.m.james92@gmail.com>
2019-07-07 00:21:15 +0100

    Added more fixtures

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 if [ -e db.sqlite3 ]
 then
-rm db.sqlite3
+    echo removing database.
+    rm db.sqlite3
 fi
 python manage.py migrate
 python manage.py loaddata comment game platform publisher user

75efa1642cdb4ae57f309fcf78385925b0b34c15
Lucas James <lucas.m.james92@gmail.com>
2019-07-07 12:23:44 +0100

    productionise - switch off debug mode

--- app/gamesapi/settings.py
+++ app/gamesapi/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6jv#&tjb3zs#$_j((+s@lo04h^pme=f1$-8^)0$)g@jt2(f04y'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
